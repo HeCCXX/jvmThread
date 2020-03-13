bc2a296e18ff51a7052430f419f2987ee5bb8e16
HeCCXX <985701799@qq.com>
2020-03-13 17:25:59 +0800

    增加注释

--- src/theThirdPackage/theThirdPackage.java
+++ src/theThirdPackage/theThirdPackage.java
@@ -6,6 +6,9 @@ package theThirdPackage;
  * @Author 贺楚翔
  * @Date 2020-03-13 17:10
  * @Version 1.0
+ *
+ * 8中基本类型：int  short  byte  float  double  boolean  char long  字面量存放在栈
+ * 包装类   String   Integer   Double   Long
  **/
 public class theThirdPackage {
     public static void main(String[] args) {
