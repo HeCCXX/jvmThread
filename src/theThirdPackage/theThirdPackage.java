1ae5b375282a293100c804e1e02e0d4bb5ce6bd5
HeCCXX <985701799@qq.com>
2020-03-13 17:17:31 +0800

    jvm堆、栈、方法区，笔记

---
+++ src/theThirdPackage/theThirdPackage.java
@@ -0,0 +1,19 @@
+package theThirdPackage;
+
+/**
+ * @ClassName theThirdPackage.theThirdPackage
+ * @Description TODO
+ * @Author 贺楚翔
+ * @Date 2020-03-13 17:10
+ * @Version 1.0
+ **/
+public class theThirdPackage {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 1;
+        Integer c = new Integer(1);
+        Integer d = new Integer(1);
+        System.out.println(a==b);
+        System.out.println(c == d);
+    }
+}
