c09aa04c9eb97b2902c0b0edc2d266eba1ff48fa
HeCCXX <985701799@qq.com>
2020-04-08 10:39:18 +0800

    String包装类字面量测试

---
+++ src/theThirdPackage/test.java
@@ -0,0 +1,24 @@
+package theThirdPackage;
+
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author 贺楚翔
+ * @Date 2020-03-18 11:03
+ * @Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        String str1 = "hcx";
+        String str2 = "hgg";
+        String str3 = str1+str2;
+        String str5 = "hcx" + "hgg";
+        String str4 = "hcxhgg";
+
+        String str6 = str1 + "hgg";
+        System.out.println(str3 == str4);
+        System.out.println(str4 == str5);
+        System.out.println(str4 == str6);
+
+    }
+}
