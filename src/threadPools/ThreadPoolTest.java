db4ee8161a87a6b7b234e6d5f8fc1137eea3e2ca
HeCCXX <985701799@qq.com>
2020-04-13 17:07:31 +0800

    线程池原理实现

---
+++ src/threadPools/ThreadPoolTest.java
@@ -0,0 +1,35 @@
+package threadPools;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName ThreadPoolTest
+ * @Description 线程池运行国过程测试，原理学习
+ * @Author 贺楚翔
+ * @Date 2020-04-13 16:22
+ * @Version 1.0
+ **/
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(5);
+        //丢弃策略，多余的丢弃，不报错
+        ThreadPoolExecutor.DiscardPolicy handler = new ThreadPoolExecutor.DiscardPolicy();
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5, 10, 60, TimeUnit.SECONDS, queue,handler);
+        for (int i = 0; i < 16; i++) {
+            poolExecutor.execute(()->{
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            System.out.println("线程池中活跃的线程数："+ poolExecutor.getPoolSize());
+            if (queue.size() > 0){
+                System.out.println("-------------------任务队列阻塞的线程数"+queue.size());
+            }
+        }
+        poolExecutor.shutdown();
+    }
+}
