9004067b593dd27f2e557b29e6413fc1efc25180
HeCCXX <985701799@qq.com>
2020-05-12 09:42:56 +0800

    快排单边循环

--- src/algorithm/QuickSort.java
+++ src/algorithm/QuickSort.java
@@ -16,7 +16,7 @@ public class QuickSort {
         if (startIndex >= endIndex){
             return;
         }
-        int pivot = partition(array,startIndex,endIndex);
+        int pivot = partition2(array,startIndex,endIndex);
         quickSort1(array,startIndex,pivot-1);
         quickSort1(array,pivot+1,endIndex);
     }
@@ -51,6 +51,30 @@ public class QuickSort {
         array[left] = pivot;
         System.out.println(Arrays.toString(array));
         return left;
+    }
+
+    /**
+    * 单边循环遍历，mark标记负责记录大于pivot和小于pivot交换的位置
+    * @param array
+    * @param startIndex
+    * @param endIndex
+    * @return int
+    * @exception
+    **/
+    private static int partition2(int[] array,int startIndex,int endIndex) {
+        int pivot = array[startIndex];
+        int mark = startIndex;
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (array[i]<pivot){
+                mark++;
+                int tmp = array[i];
+                array[i] = array[mark];
+                array[mark] = tmp;
+            }
+        }
+        array[startIndex] = array[mark];
+        array[mark] = pivot;
+        return mark;
     }
 
     public static void main(String[] args) {
