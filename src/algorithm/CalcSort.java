c0101d3912a20d8e15876beb177250127f1ce9bd
HCX <985701799@qq.com>
2020-05-13 20:40:37 +0800

    计数排序优化版

--- src/algorithm/CalcSort.java
+++ src/algorithm/CalcSort.java
@@ -35,8 +35,48 @@ public class CalcSort {
         }
         System.out.println(Arrays.toString(result));
     }
+    /**
+    * 优化计数排序，最大值减去最小值，并利用累加之前的统计，最后从数组末尾进行遍历，当出现相同的数时，统计值指定排序
+     * 是稳定的算法，不会破坏相同数的前后排序
+     * 只适用于整数、且最大值与最小值相差较小
+    * @param array
+    * @return void
+    * @exception
+    **/
+    private static void countSortBetter(int[] array) {
+
+        int maxvalue = array[0];
+        int minvalue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (maxvalue<array[i]){
+                maxvalue = array[i];
+            }
+            if (minvalue>array[i]){
+                minvalue = array[i];
+            }
+        }
+        int size = maxvalue - minvalue;
+        int[] sortArray = new int[size+1];
+        for (int i = 0; i < array.length; i++) {
+            sortArray[array[i]-minvalue]++;
+        }
+        for (int i = 1; i < sortArray.length; i++) {
+            sortArray[i] += sortArray[i-1];
+        }
+        
+        int[] result = new int[array.length];
+        for (int i = array.length-1; i >=0 ; i--) {
+            result[--sortArray[array[i]-minvalue]] = array[i];
+//            sortArray[array[i]-minvalue]--;
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+
     public static void main(String[] args) {
         int[] array = {8,3,1,6,5,2,10,7};
+        int[] array2 = {98,93,91,96,95,92,100,97};
         countSort(array);
+        countSortBetter(array2);
     }
 }
